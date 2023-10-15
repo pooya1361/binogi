9e27a11fe98be2ad1f8842eedaa19c7591045d2a
Pouya <pooya1361@gmail.com>
2023-10-15 02:25:32 +0200

    grid|list mode added

--- src/app.tsx
+++ src/app.tsx
@@ -7,6 +7,7 @@ import Linkedin from "./icons/linkedin";
 import Github from "./icons/github";
 import Twitter from "./icons/twitter";
 import { isNullOrUndefined } from "./util";
+import { faBorderAll, faGrip, faList } from "@fortawesome/free-solid-svg-icons";
 
 
 const noImageUrl = 'https://usercontent.one/wp/www.vocaleurope.eu/wp-content/uploads/no-image.jpg?media=1642546813'
@@ -18,7 +19,8 @@ const App = () => {
     const [sort, setSort] = useState<sortFilterType>("name");
     const [filterText, setFilterText] = useState<string>();
     const [filterBy, setFilterBy] = useState<sortFilterType>("name");
-    
+    const [gridList, setGridList] = useState<"grid" | "list">("list");
+
 
 
     useEffect(() => {
@@ -73,15 +75,22 @@ const App = () => {
         }
     }
 
+    const toggleGridList = () => {
+        setGridList(gridList === "grid" ? "list" : "grid")
+    }
+
     return (
         <div className="employees_page">
             <h1>The fellowship of the tretton37</h1>
             {employees ?
                 <div>
                     <div className="toolbar">
-                        <div className="flex grow">
+                        <button className="border rounded-md px-2 py-1 mr-2 bg-gray-100 w-8" onClick={toggleGridList}>
+                            <FontAwesomeIcon icon={gridList === "grid" ? faGrip : faList} />
+                        </button>
+                        <div className="flex grow w-full">
                             <div className="flex grow gap-2">
-                                <label htmlFor="filterInput" className="whitespace-nowrap text-right">Filter by:</label>
+                                <label htmlFor="filterInput" className="whitespace-nowrap text-right leading-8">Filter by:</label>
                                 <select id="filterInput" name="sort"
                                     className="block rounded-md w-36 border border-gray-300 shadow-sm focus:ring-primary-200 focus:ring-opacity-50"
                                     onChange={filterByHandle}
@@ -89,12 +98,13 @@ const App = () => {
                                     <option selected value={"name"}>Name</option>
                                     <option value={"office"}>Office</option>
                                 </select>
-                                <input className="flex grow w-full w-36 rounded-md border-gray-300 shadow-sm border focus:border-primary-200" 
+                                <input className="flex grow w-full w-36 rounded-md border-gray-300 shadow-sm border focus:border-primary-200"
                                     type="text" value={filterText} onChange={filterEmployees} />
+                                <span className="whitespace-nowrap leading-8 hidden md:block">{Number(filterText?.length) > 0 ? getFilteredEmployees()?.length + " employee(s) found" : undefined}</span>
                             </div>
                         </div>
-                        <div className="flex align gap-2 justify-normal md:justify-end">
-                            <label htmlFor="employees_sort" className="whitespace-nowrap">Sort by:</label>
+                        <div className="flex align gap-2 justify-normal md:justify-end w-full">
+                            <label htmlFor="employees_sort" className="whitespace-nowrap leading-8">Sort by:</label>
                             <select id="employees_sort" name="sort"
                                 className="block w-36 rounded-md border border-gray-300 shadow-sm focus:ring-primary-200 focus:ring-opacity-50"
                                 onChange={sortEmployees}
@@ -104,36 +114,58 @@ const App = () => {
                             </select>
                         </div>
                     </div>
-                    <div className="employees_list">
-                        {getFilteredEmployees()?.map((employee, i) =>
-                            <div className="employees_item" key={i}>
-                                <img src={employee.imagePortraitUrl ?? noImageUrl} alt="" className="block max-h-96 h-full object-cover" />
-                                <div className="flex">
-                                    <div className="flex flex-col">
-                                        <span>{employee.name}</span>
-                                        <span>Office: {employee.office}</span>
-                                    </div>
-                                    <div className="flex flex-1 justify-end items-baseline gap-1">
-                                        <Linkedin
-                                            className={`w-6 ${!isNullOrUndefined(employee.linkedIn) ? "cursor-pointer" : ""}`}
-                                            fill={isNullOrUndefined(employee.linkedIn) ? "gray" : "black"}
-                                            disabled={isNullOrUndefined(employee.linkedIn)}
-                                            onClick={() => openLink("linkedIn", employee.linkedIn)} />
-                                        <Github
-                                            className={`w-6 ${!isNullOrUndefined(employee.gitHub) ? "cursor-pointer" : ""}`}
-                                            fill={isNullOrUndefined(employee.gitHub) ? "gray" : "black"}
-                                            disabled={isNullOrUndefined(employee.gitHub)}
-                                            onClick={() => openLink("gitHub", employee.gitHub)} />
-                                        <Twitter
-                                            className={`w-6 ${!isNullOrUndefined(employee.twitter) ? "cursor-pointer" : ""}`}
-                                            fill={isNullOrUndefined(employee.twitter) ? "gray" : "black"}
-                                            disabled={isNullOrUndefined(employee.twitter)}
-                                            onClick={() => openLink("twitter", employee.twitter)} />
+                    {gridList === "grid" ?
+                        <div className="employees_grid">
+                            {getFilteredEmployees()?.map((employee, i) =>
+                                <div className="employees_grid_item" key={i}>
+                                    <img src={employee.imagePortraitUrl ?? noImageUrl} alt="" className="block max-h-96 h-full object-cover" />
+                                    <div className="flex">
+                                        <div className="flex flex-col">
+                                            <span>{employee.name}</span>
+                                            <span>Office: {employee.office}</span>
+                                        </div>
+                                        <div className="flex flex-1 justify-end items-baseline gap-1">
+                                            <Linkedin
+                                                className={`w-6 ${!isNullOrUndefined(employee.linkedIn) ? "cursor-pointer" : ""}`}
+                                                fill={isNullOrUndefined(employee.linkedIn) ? "gray" : "black"}
+                                                disabled={isNullOrUndefined(employee.linkedIn)}
+                                                onClick={() => openLink("linkedIn", employee.linkedIn)} />
+                                            <Github
+                                                className={`w-6 ${!isNullOrUndefined(employee.gitHub) ? "cursor-pointer" : ""}`}
+                                                fill={isNullOrUndefined(employee.gitHub) ? "gray" : "black"}
+                                                disabled={isNullOrUndefined(employee.gitHub)}
+                                                onClick={() => openLink("gitHub", employee.gitHub)} />
+                                            <Twitter
+                                                className={`w-6 ${!isNullOrUndefined(employee.twitter) ? "cursor-pointer" : ""}`}
+                                                fill={isNullOrUndefined(employee.twitter) ? "gray" : "black"}
+                                                disabled={isNullOrUndefined(employee.twitter)}
+                                                onClick={() => openLink("twitter", employee.twitter)} />
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        )}
-                    </div>
+                            )}
+                        </div>
+                        :
+                        <div className="employees_list">
+                            {getFilteredEmployees()?.map((employee, i) =>
+
+                                <div className="employee_list_item">
+                                    <img src={employee.imagePortraitUrl ?? noImageUrl} alt="" className="block max-w-20 w-20 h-full object-cover" />
+                                    <div className="grid grid-cols-2 gap-2 w-full">
+                                        <div className="flex flex-col gap-2">
+                                            <span>{employee.name}</span>
+                                            <span>Office: {employee.office}</span>
+                                        </div>
+                                        <div className="flex flex-col gap-2">
+                                            <span>Area: {employee.area}</span>
+                                            <span>Role: {employee.primaryRole}</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            )}
+
+                        </div>
+                    }
                 </div>
                 : (
                     <div className="flex flex-1 min-h-full min-w-full ">
